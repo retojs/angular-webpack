63a1018dd0f51c3c5ff6c44e198d1ab998de88b0
Reto Lamprecht <reto.lamprecht@swisscom.com>
2018-01-09 11:10:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# A front-end project template using Angular 5, Webpack, SASS and Jest.
+
+## Usage
+
+`$ ./tsc` compiles typescript files
+
+`$ npm run build` runs the webpack build
+
+`$ npm start` starts the webpack-dev-server
+
+
+### Inspired by
+	* https://semaphoreci.com/community/tutorials/setting-up-angular-2-with-webpack
+	* https://www.xfive.co/blog/testing-angular-faster-jest/
+	
---
+++ webpack.dev.js
@@ -0,0 +1,93 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const path = require('path');
+const rootDir = path.resolve(__dirname);
+const helpers = {
+    root: function root(args) {
+        args = Array.prototype.slice.call(arguments, 0);
+        return path.join.apply(path, [rootDir].concat(args));
+    }
+};
+
+module.exports = {
+
+    devtool: 'cheap-module-eval-source-map',
+
+    entry: {
+        'polyfills': './src/polyfills.ts',
+        'vendor': './src/vendor.ts',
+        'app': './src/main.ts'
+    },
+
+    output: {
+        path: helpers.root('dist'),
+        publicPath: '/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: {configFileName: helpers.root('tsconfig.json')}
+                    }, 'angular2-template-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+            },
+            {
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'),
+                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader?sourceMap']})
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('src', 'app'),
+                loader: 'raw-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin('[name].css'),
+
+        // Workaround for angular/angular#11580
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)@angular/,
+            helpers.root('./src'), // location of your src
+            {} // a map of your routes
+        ),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
+
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
+};
